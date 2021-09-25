2088c55c62626baa915d8d0a61c067a87a24c81b
Rohan Modi <rohan.a.c.modi@gmail.com>
2021-09-24 23:19:08 -0400

    Issue #1: Added file name parameter

--- autoscan.py
+++ autoscan.py
@@ -1,2 +1,16 @@
+#!/usr/bin/python
+
+import sys
+import os
+
 print ('Searching for documents...')
 
+
+if len(sys.argv) > 1:
+    fileName = sys.argv[1]
+    if os.path.isfile('./'+fileName):
+        print("Found file")
+    else:
+        print("Could not find file")
+else:
+    print("Please specify name of file to parse")
