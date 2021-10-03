91a720070c33fe7ec9ef6dca3f1b87e469a80e7e
Rohan Modi <rohan.a.c.modi@gmail.com>
2021-10-02 20:37:51 -0400

    Issue #6: If argument is directory, print contents

--- autoscan.py
+++ autoscan.py
@@ -3,15 +3,18 @@
 import sys
 import os
 
+userInput = sys.argv[1]
+
 print ('Searching for documents...')
 
 if len(sys.argv) == 1:
     print("Please specify name of file to parse.")
 elif len(sys.argv) > 2:
     print("Too many arguments. Please specify name of file to parse.")
+elif os.path.isdir(userInput):
+    print(os.listdir(userInput))
 else:
-    fileName = sys.argv[1]
-    if os.path.isfile(fileName):
-        print("Parsing " + fileName)
+    if os.path.isfile(userInput):
+        print("Parsing " + userInput)
     else:
-        print("Could not find file " + fileName)
+        print("Could not find file or directory  " + userInput)
