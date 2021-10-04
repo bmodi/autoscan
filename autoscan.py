10a9ba91ef3646d5937f839ea4c44d139e9d3d69
Rohan Modi <rohan.a.c.modi@gmail.com>
2021-10-04 19:56:42 -0400

    Issue #5: Use pdfplumber to print text from PDF, organize code into functions

--- autoscan.py
+++ autoscan.py
@@ -2,19 +2,35 @@
 
 import sys
 import os
+import pdfplumber
 
-userInput = sys.argv[1]
+def getInput():
+    userInput = sys.argv[1]
+    print("User input is " + userInput)
+    if len(sys.argv) == 1:
+        print("Please specify name of file to parse.")
+    elif len(sys.argv) > 2:
+        print("Too many arguments. Please specify name of file to parse.")
+    return userInput
 
-print ('Searching for documents...')
+def printFilesInDirectory(directoryName):
+    listOfFiles = os.listdir(directoryName)
+    directoryContainsPDF = False
+    for file in listOfFiles:
+        if file.endswith('pdf'):
+            print(file)
+            directoryContainsPDF = True
+    if directoryContainsPDF == False:
+        print("Directory contains no PDFs")
 
-if len(sys.argv) == 1:
-    print("Please specify name of file to parse.")
-elif len(sys.argv) > 2:
-    print("Too many arguments. Please specify name of file to parse.")
-elif os.path.isdir(userInput):
-    print(os.listdir(userInput))
+input = getInput()
+
+if os.path.isdir(input):
+    printFilesInDirectory(input)
+elif os.path.isfile(input):
+    print("Parsing " + input)
+    with pdfplumber.open(input) as pdf:
+        first_page = pdf.pages[0]
+        print(first_page.extract_text())
 else:
-    if os.path.isfile(userInput):
-        print("Parsing " + userInput)
-    else:
-        print("Could not find file or directory  " + userInput)
+    print("Could not find file or directory  " + input)
